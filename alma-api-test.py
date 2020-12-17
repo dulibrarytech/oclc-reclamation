be530d7d574fb76cd85af2b990e4cb3f229ed277
Scott Salvaggio <scottsalvaggio@gmail.com>
2020-12-16 21:13:36 -0800

    Add logging

--- alma-api-test.py
+++ alma-api-test.py
@@ -1,29 +1,40 @@
+import logging
+import logging.config
 import os
 import pandas as pd
 import requests
 import xml.etree.ElementTree as ET
-from xml.dom import minidom
 from dotenv import load_dotenv
+from requests.exceptions import HTTPError
+from xml.dom import minidom
 
 load_dotenv()
 
+logging.config.fileConfig('logging.conf', disable_existing_loggers=False)
+logger = logging.getLogger(__name__)
+
 API_URL = os.getenv('API_URL')
-mms_id = '991017143999702766'
-oclc_num = '12345678 ' # <-- has 'ocm' prefix'; '123456789' <-- has 'ocn' prefix
-params = {'view': 'full'}
 API_KEY = os.getenv('API_KEY')
 headers = {'Authorization': f'apikey {API_KEY}'}
+params = {'view': 'full'}
 
 def get_alma_record(mms_id):
     """GET record based on MMS ID. Return root element of parsed XML tree."""
-    response = requests.get(API_URL + mms_id, params=params, headers=headers,
-        timeout=45)
 
-    print('\nGET reponse:', response)
-    print('Request URL:', response.url)
-    print('Status:', response.status_code)
-    print('Raise for status:', response.raise_for_status())
-    print('Encoding:', response.encoding)
+    try:
+        response = requests.get(f'{API_URL}{mms_id}', params=params, headers=headers,
+            timeout=45)
+        logger.debug(f'GET reponse: {response}')
+        logger.debug(f'Request URL: {response.url}')
+        logger.debug(f'Status: {response.status_code}')
+        logger.debug(f'Encoding: {response.encoding}')
+        response.raise_for_status()
+    except HTTPError as http_err:
+        logger.exception(f'HTTP error occurred: {http_err}')
+        return -1
+    except Exception as err:
+        logger.exception(f'Error occurred: {err}')
+        return -1
 
     # Pretty-print XML response
     xml_as_pretty_printed_str = \
@@ -65,6 +76,10 @@ def update_alma_record(mms_id, oclc_num):
     print('Full OCLC number:', full_oclc_num)
 
     root = get_alma_record(mms_id)
+
+    if root == -1:
+        return -1
+
     record_element = root.find('./record')
 
     # Get index of first 035 element
@@ -100,14 +115,21 @@ def update_alma_record(mms_id, oclc_num):
     # Send PUT request
     headers['Content-Type'] = 'application/xml'
     payload = ET.tostring(root, encoding='UTF-8')
-    put_response = requests.put(API_URL + mms_id, headers=headers,
-        data=payload, timeout=45)
 
-    print('\nPUT reponse:', put_response)
-    print('Request URL:', put_response.url)
-    print('Status:', put_response.status_code)
-    print('Raise for status:', put_response.raise_for_status())
-    print('Encoding:', put_response.encoding)
+    try:
+        put_response = requests.put(f'{API_URL}{mms_id}', headers=headers,
+            data=payload, timeout=45)
+        logger.debug(f'PUT reponse: {put_response}')
+        logger.debug(f'Request URL: {put_response.url}')
+        logger.debug(f'Status: {put_response.status_code}')
+        logger.debug(f'Encoding: {put_response.encoding}')
+        put_response.raise_for_status()
+    except HTTPError as http_err:
+        logger.exception(f'HTTP error occurred: {http_err}')
+        return -1
+    except Exception as err:
+        logger.exception(f'Error occurred: {err}')
+        return -1
 
     # Pretty-print XML response
     xml_as_pretty_printed_str = \
