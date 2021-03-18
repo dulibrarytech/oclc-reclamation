842d4a03665dcde0308f9573249ed4da7e9da06e
Scott Salvaggio <scottsalvaggio@gmail.com>
2021-03-17 22:00:19 -0700

    Check all 035 fields and insert/remove 035 fields as appropriate

--- update_alma_records.py
+++ update_alma_records.py
@@ -3,6 +3,7 @@ import logging
 import logging.config
 import os
 import pandas as pd
+import re
 import requests
 import xml.etree.ElementTree as ET
 from dotenv import load_dotenv
@@ -95,51 +96,138 @@ def update_alma_record(mms_id: str, oclc_num: str) -> None:
     # TO DO: Consider checking all 035 elements for (OCoLC) prefix (instead of
     # just the first element). Note that you will have to do this for the
     # script that updates the OCLC number.
-    assert not record_element[first_035_element_index][0].text.startswith(
-        '(OCoLC)'), \
-        f'This record already has an OCLC number: ' \
-        f'"{record_element[first_035_element_index][0].text}".'
+    # assert not record_element[first_035_element_index][0].text.startswith(
+    #     '(OCoLC)'), \
+    #     f'This record already has an OCLC number: ' \
+    #     f'"{record_element[first_035_element_index][0].text}".'
 
-    # Create new 035 element with OCLC number
-    new_035_element = ET.Element('datafield')
-    new_035_element.set('ind1', ' ')
-    new_035_element.set('ind2', ' ')
-    new_035_element.set('tag', '035')
-    sub_element = ET.SubElement(new_035_element, 'subfield')
-    sub_element.set('code', 'a')
-    sub_element.text = full_oclc_num
+    need_to_update_record = False
+    oclc_nums_from_record = list()
+    oclc_nums_for_019_field = set()
+    found_035_field_with_current_oclc_num = False
 
-    # Insert new 035 element into XML
-    record_element.insert(first_035_element_index, new_035_element)
+    # Iterate over each 035 field
+    for i, element in enumerate(
+        record_element.findall('./datafield[@tag="035"]')):
+        # Extract subfield a (which would contain the OCLC number if present)
+        subfield_a = element.find('./subfield[@code="a"]').text
+        logger.debug(f'035 field #{i}, subfield a: {subfield_a}')
 
-    first_035_element_as_str = \
-        ET.tostring(record_element[first_035_element_index], encoding="unicode")
-    logger.debug(f'First 035 element after insert:\n{first_035_element_as_str}')
+        # Skip this 035 field if it's not an OCLC number
+        if not subfield_a.startswith('(OCoLC)'):
+            continue
 
-    # Send PUT request
-    headers['Content-Type'] = 'application/xml'
-    payload = ET.tostring(root, encoding='UTF-8')
+        # Extract the OCLC number itself
+        match_on_first_digit = re.search(r'\d', subfield_a)
 
-    put_response = requests.put(f'{API_URL}{mms_id}', headers=headers,
-        data=payload, timeout=45)
-    logger.debug(f'PUT reponse: {put_response}')
-    logger.debug(f'Request URL: {put_response.url}')
-    logger.debug(f'Status: {put_response.status_code}')
-    logger.debug(f'Encoding: {put_response.encoding}')
-    put_response.raise_for_status()
+        # TO DO: Decide if this should be an assertion, which prevents further
+        # processing of the record.
+        assert match_on_first_digit, f'This record contains an 035 field ' \
+            f'with an OCLC number that has no digits ({subfield_a}).'
 
-    # Pretty-print XML response
-    xml_as_pretty_printed_str = \
-        minidom.parseString(put_response.text).toprettyxml(indent='  ',
-        encoding='UTF-8')
-    logger.debug(f'Modified record:\n' \
-        f'{xml_as_pretty_printed_str.decode("UTF-8")}')
+        extracted_oclc_num_from_record = \
+            subfield_a[match_on_first_digit.start():]
+        logger.debug(f'035 field #{i}, extracted OCLC number: ' \
+            f'{extracted_oclc_num_from_record}')
 
-    # Create XML file
-    with open(f'xml/{mms_id}_modified.xml', 'wb') as file:
-        file.write(xml_as_pretty_printed_str)
+        oclc_nums_from_record.append(extracted_oclc_num_from_record)
 
-    logger.debug(f'MMS ID "{mms_id}" has been updated.')
+        # Compare the extracted OCLC number to the current OCLC number
+        extracted_oclc_num_matches_current_oclc_num = \
+            oclc_num.strip() == extracted_oclc_num_from_record.strip()
+        logger.debug(f'Does the extracted OCLC number ' \
+            f'({extracted_oclc_num_from_record}) match the current OCLC ' \
+            f'number ({oclc_num})? ' \
+            f'{extracted_oclc_num_matches_current_oclc_num}')
+
+        # (not A or (B and A)) can be simplified to (not A or B) because the
+        # second operand (B and A) is not evaluated when the boolean expression
+        # can be determined from the first operand (not A) alone. This is called
+        # short-circuit (lazy) evaluation, which is used by Python.
+        # Therefore:
+        #     not extracted_oclc_num_matches_current_oclc_num or (
+        #        found_035_field_with_current_oclc_num and
+        #        extracted_oclc_num_matches_current_oclc_num
+        #     )
+        # can be simplified to:
+        #     not extracted_oclc_num_matches_current_oclc_num or
+        #     found_035_field_with_current_oclc_num
+        if (not extracted_oclc_num_matches_current_oclc_num or
+            found_035_field_with_current_oclc_num):
+            # This 035 field either contains an old OCLC number or is a
+            # duplicate of another 035 field with the current OCLC number.
+            # In either case, remove this 035 field.
+            record_element.remove(element)
+
+            if not extracted_oclc_num_matches_current_oclc_num:
+                oclc_nums_for_019_field.add(extracted_oclc_num_from_record)
+
+            need_to_update_record = True
+        else:
+            found_035_field_with_current_oclc_num = True
+
+    # for i, extracted_oclc_num in enumerate(oclc_nums_from_record):
+    #     logger.debug(f'Comparing oclc_nums_from_record[{i}] = ' \
+    #         f'{extracted_oclc_num} to current OCLC number = {oclc_num}.')
+    #
+    #     if oclc_num != extracted_oclc_num:
+    #         oclc_nums_for_019_field.add(extracted_oclc_num)
+    #     else:
+    #         found_035_field_with_current_oclc_num = True
+
+    if len(oclc_nums_from_record) > 1:
+        oclc_nums_str = '\n'.join(oclc_nums_from_record)
+        logger.warning(f'Original record contained more than one 035 field ' \
+            f'with an (OCoLC) prefix. Here are the OCLC numbers from the ' \
+            f'original record:\n{oclc_nums_str}\nHere is the same list in another format: {oclc_nums_from_record}.')
+
+    # TO DO: Add old OCLC numbers to 019 field
+    logger.debug(f'{oclc_nums_for_019_field=}')
+
+    if not found_035_field_with_current_oclc_num:
+        # Create new 035 element with OCLC number
+        new_035_element = ET.Element('datafield')
+        new_035_element.set('ind1', ' ')
+        new_035_element.set('ind2', ' ')
+        new_035_element.set('tag', '035')
+        sub_element = ET.SubElement(new_035_element, 'subfield')
+        sub_element.set('code', 'a')
+        sub_element.text = full_oclc_num
+
+        # Insert new 035 element into XML
+        record_element.insert(first_035_element_index, new_035_element)
+
+        first_035_element_as_str = \
+            ET.tostring(record_element[first_035_element_index], encoding="unicode")
+        logger.debug(f'First 035 element after insert:\n{first_035_element_as_str}')
+
+    if need_to_update_record:
+        # Send PUT request
+        headers['Content-Type'] = 'application/xml'
+        payload = ET.tostring(root, encoding='UTF-8')
+
+        put_response = requests.put(f'{API_URL}{mms_id}', headers=headers,
+            data=payload, timeout=45)
+        logger.debug(f'PUT reponse: {put_response}')
+        logger.debug(f'Request URL: {put_response.url}')
+        logger.debug(f'Status: {put_response.status_code}')
+        logger.debug(f'Encoding: {put_response.encoding}')
+        put_response.raise_for_status()
+
+        # Pretty-print XML response
+        xml_as_pretty_printed_str = \
+            minidom.parseString(put_response.text).toprettyxml(indent='  ',
+            encoding='UTF-8')
+        logger.debug(f'Modified record:\n' \
+            f'{xml_as_pretty_printed_str.decode("UTF-8")}')
+
+        # Create XML file
+        with open(f'xml/{mms_id}_modified.xml', 'wb') as file:
+            file.write(xml_as_pretty_printed_str)
+
+        logger.debug(f'MMS ID "{mms_id}" has been updated.')
+    else:
+        logger.debug(f'No update needed for MMS ID "{mms_id}".')
 
 def init_argparse() -> argparse.ArgumentParser:
     """Initialize and return ArgumentParser object."""
