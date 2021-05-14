65ddec8f4bac104529025b56af139c4a440b66e6
Scott Salvaggio <scottsalvaggio@gmail.com>
2021-05-14 15:14:39 -0700

    Simplify API response debugging statements

--- libraries/api.py
+++ libraries/api.py
@@ -7,8 +7,8 @@ logger = logging.getLogger(__name__)
 
 def log_response_and_raise_for_status(
     response: requests.models.Response) -> None:
-    logger.debug(f'API Response: {response}')
-    logger.debug(f'Request URL: {response.url}')
-    logger.debug(f'Status: {response.status_code}')
-    logger.debug(f'Encoding: {response.encoding}')
+    logger.debug(f'API response details:\n' \
+        f'\t- URL: {response.url}\n' \
+        f'\t- HTTP status code: {response.status_code}\n' \
+        f'\t- Encoding: {response.encoding}')
     response.raise_for_status()
