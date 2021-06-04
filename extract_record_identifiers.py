bfec9e641d47b3b1039e62ca4b510bf2473386f1
Scott Salvaggio <scottsalvaggio@gmail.com>
2021-06-04 14:40:48 -0700

    Add indentation and line breaks when needed to improve readability. Use parentheses to wrap strings broken over multiple lines (instead of using a backslash).

--- extract_record_identifiers.py
+++ extract_record_identifiers.py
@@ -15,11 +15,11 @@ def init_argparse() -> argparse.ArgumentParser:
     """Initializes and returns ArgumentParser object."""
 
     parser = argparse.ArgumentParser(
-        usage='%(prog)s [option] directory_with_xml_files ' \
-            '[alma_records_with_current_oclc_num]',
-        description=f'For each XML file in the directory, extract the MMS ID ' \
-            f'and OCLC Number(s) from each Alma record and append them to ' \
-            f'the appropriate master_list_records CSV file.',
+        usage=('%(prog)s [option] directory_with_xml_files '
+            '[alma_records_with_current_oclc_num]'),
+        description=('For each XML file in the directory, extract the MMS ID '
+            'and OCLC Number(s) from each Alma record and append them to '
+            'the appropriate master_list_records CSV file.'),
     )
     parser.add_argument(
         '-v', '--version', action='version',
@@ -29,7 +29,7 @@ def init_argparse() -> argparse.ArgumentParser:
         'Directory_with_xml_files',
         metavar='directory_with_xml_files',
         type=str,
-        help=f'the path to the directory containing the XML files to process'
+        help='the path to the directory containing the XML files to process'
     )
     parser.add_argument(
         'Alma_records_with_current_oclc_num',
@@ -37,9 +37,9 @@ def init_argparse() -> argparse.ArgumentParser:
         nargs='?',
         const=None,
         type=str,
-        help=f'the name and path of the CSV file containing the MMS IDs of ' \
-            f'all Alma records with a current OCLC number (e.g. ' \
-            f'csv/alma_records_with_current_oclc_num.csv)'
+        help=('the name and path of the CSV file containing the MMS IDs of '
+            'all Alma records with a current OCLC number (e.g. '
+            'csv/alma_records_with_current_oclc_num.csv)')
     )
     return parser
 
@@ -65,7 +65,7 @@ def main() -> None:
     alma_records_with_current_oclc_num = set()
     if args.Alma_records_with_current_oclc_num is not None:
         with open(args.Alma_records_with_current_oclc_num, mode='r',
-            newline='') as file:
+                newline='') as file:
             file_reader = reader(file)
             for mms_id in file_reader:
                 alma_records_with_current_oclc_num.add(mms_id[0])
@@ -119,7 +119,7 @@ def main() -> None:
                 found_error_in_record = False
 
                 for field_035_element_index, field_035_element in enumerate(
-                    record_element.findall('./datafield[@tag="035"]')):
+                        record_element.findall('./datafield[@tag="035"]')):
                     # Extract subfield a (which would contain the OCLC number
                     # if present)
                     subfield_a_with_oclc_num = \
@@ -154,20 +154,20 @@ def main() -> None:
 
                 if unique_oclc_nums_from_record_len == 0:
                     unique_oclc_nums_from_record_str = '<none>'
-                    logger.debug(f'{mms_id} has no OCLC number in an 035 $a ' \
+                    logger.debug(f'{mms_id} has no OCLC number in an 035 $a '
                         f'field')
                     found_error_in_record = True
                 elif unique_oclc_nums_from_record_len == 1:
                     unique_oclc_nums_from_record_str = \
                         next(iter(unique_oclc_nums_from_record))
                     if found_error_in_record:
-                        logger.debug(f'{mms_id} has at least one invalid ' \
+                        logger.debug(f'{mms_id} has at least one invalid '
                             f'OCLC number: {unique_oclc_nums_from_record_str}')
                 else:
                     # unique_oclc_nums_from_record_len > 1
                     unique_oclc_nums_from_record_str = \
                         ', '.join(unique_oclc_nums_from_record)
-                    logger.debug(f'{mms_id} has multiple OCLC numbers: ' \
+                    logger.debug(f'{mms_id} has multiple OCLC numbers: '
                         f'{unique_oclc_nums_from_record_str}')
                     found_error_in_record = True
 
@@ -175,14 +175,18 @@ def main() -> None:
                     # Add record to records_with_errors spreadsheet
                     if records_with_errors.tell() == 0:
                         # Write header row
-                        records_with_errors_writer.writerow([ 'MMS ID',
+                        records_with_errors_writer.writerow([
+                            'MMS ID',
                             "Unique OCLC Number(s) from Alma Record's 035 $a",
-                            "All OCLC Numbers from Alma Record's 035 $a" ])
+                            "All OCLC Numbers from Alma Record's 035 $a"
+                        ])
 
-                    records_with_errors_writer.writerow([ mms_id,
+                    records_with_errors_writer.writerow([
+                        mms_id,
                         unique_oclc_nums_from_record_str,
                         '<none>' if len(all_oclc_nums_from_record) == 0
-                        else ', '.join(all_oclc_nums_from_record) ])
+                            else ', '.join(all_oclc_nums_from_record)
+                    ])
                 elif mms_id in alma_records_with_current_oclc_num:
                     logger.debug(f'{mms_id} has current OCLC number')
 
@@ -190,10 +194,14 @@ def main() -> None:
                     if records_with_current_oclc_num.tell() == 0:
                         # Write header row
                         records_with_current_oclc_num_writer.writerow([
-                            'MMS ID', 'Current OCLC Number' ])
+                            'MMS ID',
+                            'Current OCLC Number'
+                        ])
 
-                    records_with_current_oclc_num_writer.writerow([ mms_id,
-                        unique_oclc_nums_from_record_str ])
+                    records_with_current_oclc_num_writer.writerow([
+                        mms_id,
+                        unique_oclc_nums_from_record_str
+                    ])
                 else:
                     logger.debug(f'{mms_id} has a potentially old OCLC number')
 
@@ -203,12 +211,15 @@ def main() -> None:
                         records_with_potentially_old_oclc_num_writer.writerow([
                             'MMS ID',
                             "Unique OCLC Number from Alma Record's 035 $a",
-                            "All OCLC Numbers from Alma Record's 035 $a" ])
+                            "All OCLC Numbers from Alma Record's 035 $a"
+                        ])
 
                     records_with_potentially_old_oclc_num_writer.writerow([
-                        mms_id, unique_oclc_nums_from_record_str,
+                        mms_id,
+                        unique_oclc_nums_from_record_str,
                         '<none>' if len(all_oclc_nums_from_record) == 0
-                        else ', '.join(all_oclc_nums_from_record) ])
+                            else ', '.join(all_oclc_nums_from_record)
+                    ])
 
                 logger.debug(f'Finished processing MMS ID {mms_id}\n')
 
