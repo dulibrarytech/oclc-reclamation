cb7668cda7cc5f8632386c549fdcd991301515a1
Scott Salvaggio <scottsalvaggio@gmail.com>
2021-06-04 19:47:05 -0700

    Edit extract_record_identifiers.py to handle case where an 035 field has zero or more than one subfield a values

--- extract_record_identifiers.py
+++ extract_record_identifiers.py
@@ -117,17 +117,32 @@ def main() -> None:
                 all_oclc_nums_from_record = list()
                 unique_oclc_nums_from_record = set()
                 found_error_in_record = False
+                contains_invalid_number_of_subfield_a_values_in_035 = False
+                error_msg = None
 
                 for field_035_element_index, field_035_element in enumerate(
                         record_element.findall('./datafield[@tag="035"]')):
                     # Extract subfield a (which would contain the OCLC number
                     # if present)
-                    subfield_a_with_oclc_num = \
+                    subfield_a_data = \
                         libraries.record.get_subfield_a_with_oclc_num(
                             field_035_element,
                             field_035_element_index)
 
-                    if subfield_a_with_oclc_num is None:
+                    # Handle case where 035 field does not contain a single
+                    # subfield $a
+                    if subfield_a_data.subfield_a_count != 1:
+                        # Stop checking this record's 035 fields, but store
+                        # the error message
+                        contains_invalid_number_of_subfield_a_values_in_035 = \
+                            True
+                        error_msg = subfield_a_data.error_msg
+                        break
+
+                    if (subfield_a_data.subfield_a_count == 1
+                            and subfield_a_data.string_with_oclc_num is None):
+                        # This 035 field does not contain an OCLC number, so
+                        # skip it
                         continue
 
                     (subfield_a_without_oclc_org_code_prefix,
@@ -136,7 +151,7 @@ def main() -> None:
                             found_valid_oclc_num,
                             found_error_in_record) = \
                         libraries.record.extract_oclc_num_from_subfield_a(
-                            subfield_a_with_oclc_num,
+                            subfield_a_data.string_with_oclc_num,
                             field_035_element_index,
                             found_error_in_record)
 
@@ -145,31 +160,35 @@ def main() -> None:
 
                     unique_oclc_nums_from_record.add(extracted_oclc_num)
 
-                logger.debug(f'{unique_oclc_nums_from_record=}')
-                logger.debug(f'{all_oclc_nums_from_record=}')
-
                 unique_oclc_nums_from_record_len = \
                     len(unique_oclc_nums_from_record)
                 unique_oclc_nums_from_record_str = None
 
-                if unique_oclc_nums_from_record_len == 0:
-                    unique_oclc_nums_from_record_str = '<none>'
-                    logger.debug(f'{mms_id} has no OCLC number in an 035 $a '
-                        f'field')
+                if contains_invalid_number_of_subfield_a_values_in_035:
                     found_error_in_record = True
-                elif unique_oclc_nums_from_record_len == 1:
-                    unique_oclc_nums_from_record_str = \
-                        next(iter(unique_oclc_nums_from_record))
-                    if found_error_in_record:
-                        logger.debug(f'{mms_id} has at least one invalid '
-                            f'OCLC number: {unique_oclc_nums_from_record_str}')
                 else:
-                    # unique_oclc_nums_from_record_len > 1
-                    unique_oclc_nums_from_record_str = \
-                        ', '.join(unique_oclc_nums_from_record)
-                    logger.debug(f'{mms_id} has multiple OCLC numbers: '
-                        f'{unique_oclc_nums_from_record_str}')
-                    found_error_in_record = True
+                    logger.debug(f'{unique_oclc_nums_from_record=}')
+                    logger.debug(f'{all_oclc_nums_from_record=}')
+
+                    if unique_oclc_nums_from_record_len == 0:
+                        unique_oclc_nums_from_record_str = '<none>'
+                        logger.debug(f'{mms_id} has no OCLC number in an 035 '
+                            f'$a field')
+                        found_error_in_record = True
+                    elif unique_oclc_nums_from_record_len == 1:
+                        unique_oclc_nums_from_record_str = \
+                            next(iter(unique_oclc_nums_from_record))
+                        if found_error_in_record:
+                            logger.debug(f'{mms_id} has at least one invalid '
+                                f'OCLC number: '
+                                f'{unique_oclc_nums_from_record_str}')
+                    else:
+                        # unique_oclc_nums_from_record_len > 1
+                        unique_oclc_nums_from_record_str = \
+                            ', '.join(unique_oclc_nums_from_record)
+                        logger.debug(f'{mms_id} has multiple OCLC numbers: '
+                            f'{unique_oclc_nums_from_record_str}')
+                        found_error_in_record = True
 
                 if found_error_in_record:
                     # Add record to records_with_errors spreadsheet
@@ -178,15 +197,25 @@ def main() -> None:
                         records_with_errors_writer.writerow([
                             'MMS ID',
                             "Unique OCLC Number(s) from Alma Record's 035 $a",
-                            "All OCLC Numbers from Alma Record's 035 $a"
+                            "All OCLC Numbers from Alma Record's 035 $a",
+                            'Error'
                         ])
 
-                    records_with_errors_writer.writerow([
-                        mms_id,
-                        unique_oclc_nums_from_record_str,
-                        '<none>' if len(all_oclc_nums_from_record) == 0
-                            else ', '.join(all_oclc_nums_from_record)
-                    ])
+                    if contains_invalid_number_of_subfield_a_values_in_035:
+                        records_with_errors_writer.writerow([
+                            mms_id,
+                            '<record not fully checked>',
+                            '<record not fully checked>',
+                            error_msg
+                        ])
+                    else:
+                        records_with_errors_writer.writerow([
+                            mms_id,
+                            unique_oclc_nums_from_record_str,
+                            '<none>' if len(all_oclc_nums_from_record) == 0
+                                else ', '.join(all_oclc_nums_from_record),
+                            error_msg
+                        ])
                 elif mms_id in alma_records_with_current_oclc_num:
                     logger.debug(f'{mms_id} has current OCLC number')
 
