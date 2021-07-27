f79bba143cece1427d8931e61cc89f6cec013690
Scott Salvaggio <scottsalvaggio@gmail.com>
2021-07-26 18:19:05 -0700

    Add warning statements to log

--- libraries/handle_file.py
+++ libraries/handle_file.py
@@ -13,12 +13,21 @@ def csv_column_to_set(path_to_csv: str, target_set: Set[str],
     if path_to_csv is not None:
         with open(path_to_csv, mode='r', newline='') as file:
             file_reader = reader(file)
-            for row in file_reader:
+            for row_index, row in enumerate(file_reader, start=1):
                 value = row[col_num]
 
                 if isinstance(value, str):
                     value = value.strip()
-                    if not keep_leading_zeros and value.isdigit():
-                        value = libraries.record.remove_leading_zeros(value)
+                    if value.isdigit():
+                        if not keep_leading_zeros:
+                            value = libraries.record.remove_leading_zeros(value)
+                    else:
+                        logger.warning(f'{path_to_csv}, row #{row_index} '
+                            f'contains a value with at least one non-digit '
+                            f'character: {value}')
+                else:
+                    logger.warning(f'{path_to_csv}, row #{row_index} contains '
+                        f'the value "{value}", which is not a string, but '
+                        f'rather of type: {type(value)}')
 
                 target_set.add(value)
